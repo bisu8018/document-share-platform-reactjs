b84fc190599e9f1d52f3db5a79db371dacedf806
centroine <centroine@naver.com>
2018-10-04 17:51:05 +0900

    document registry migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const MyStringStore = artifacts.require("MyStringStore");
+const DocumentRegistry = artifacts.require("DocumentRegistry");
+const RewardPool = artifacts.require("RewardPool");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
+  deployer.deploy(DocumentRegistry);
+  deployer.deploy(RewardPool);
 };
