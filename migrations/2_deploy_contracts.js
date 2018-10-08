d9412968a8ec83d160debdc75389a0db8616e26e
centroine <centroine@naver.com>
2018-10-08 21:45:15 +0900

    smart contract migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const MyStringStore = artifacts.require("MyStringStore");
 const Deck = artifacts.require("Deck");
-const DocumentRegistry = artifacts.require("DocumentRegistry");
+const Utility = artifacts.require("Utility");
+const DocumentReg = artifacts.require("DocumentReg");
+const AuthorPool = artifacts.require("AuthorPool");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
   deployer.deploy(Deck);
-  deployer.deploy(DocumentRegistry);
+  deployer.deploy(Utility);
+  deployer.deploy(DocumentReg);
+  deployer.deploy(AuthorPool);
 };
