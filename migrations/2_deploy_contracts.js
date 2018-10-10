e134327aed96e54d2703627734c08f3332f357c0
centroine <centroine@naver.com>
2018-10-10 20:52:29 +0900

    smart contract - missing files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
-const MyStringStore = artifacts.require("MyStringStore");
 const Deck = artifacts.require("Deck");
 const Utility = artifacts.require("Utility");
 const DocumentReg = artifacts.require("DocumentReg");
 const AuthorPool = artifacts.require("AuthorPool");
 
 module.exports = function(deployer) {
-  deployer.deploy(MyStringStore);
   deployer.deploy(Deck);
   deployer.deploy(Utility);
   deployer.deploy(DocumentReg);
