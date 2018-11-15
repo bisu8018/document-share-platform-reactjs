0f7481ab126b3f1f22f90389da10d60774ba40f8
MacDev <macdev@MacDevui-MacBook-Pro.local>
2018-11-15 00:47:42 -0800

    jay env에 따른 properties처리

--- client/src/resources/app.properties.js
+++ client/src/resources/app.properties.js
@@ -1,36 +1,31 @@
 export const APP_PROPERTIES = {
 
-  env: (process.env.NODE_ENV) ? 'local':process.env.NODE_ENV,
-  mainHost: 'https://share.decompany.io',
-  //mainHost: 'http://localhost:8000',
-  domain:{
-    upload: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com',
-    //image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
-    image: 'http://dev-ca-document.s3-website-us-west-1.amazonaws.com',
-    //api: "http://localhost:4000",
-    api: 'https://6chcj5b70g.execute-api.us-west-1.amazonaws.com/dev'
-  },
-  local:{
-    domain:{
-      upload: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com',
-      image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
-      api: "http://localhost:4000",
+  env: (!process.env.NODE_ENV) ? 'local':process.env.NODE_ENV,
+  domain:function(){
+    if(process.env.NODE_ENV === 'production'){
+      return APP_PROPERTIES.production.domain;
+    }else {
+      return APP_PROPERTIES.local.domain;
     }
   },
-  dev:{
+  local:{
+    mainHost: 'http://localhost:8000',
     domain:{
       upload: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com',
-      image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
+      //image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
+      image: 'http://dev-ca-document.s3-website-us-west-1.amazonaws.com',
       //api: "http://localhost:4000",
-      api: 'https://iwzx8ah5xf.execute-api.us-west-1.amazonaws.com/dev'
+      api: 'https://6chcj5b70g.execute-api.us-west-1.amazonaws.com/dev'
     }
   },
   production:{
+    mainHost: 'https://share.decompany.io',
     domain:{
       upload: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com',
-      image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
+      //image: 'https://24gvmjxwme.execute-api.us-west-1.amazonaws.com/prod',
+      image: 'http://dev-ca-document.s3-website-us-west-1.amazonaws.com',
       //api: "http://localhost:4000",
-      api: 'https://iwzx8ah5xf.execute-api.us-west-1.amazonaws.com/dev'
+      api: 'https://6chcj5b70g.execute-api.us-west-1.amazonaws.com/dev'
     }
   },
   service_endpoint:{
